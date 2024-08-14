a7c18f6b60b9029dcd0dc0e3c4f2c0a4ebf873e1
IndraMatsiendra <imcdindra@hotmail.com>
2024-08-14 00:47:56 -0300

    correções de erros de seg fault

--- src/pager.c
+++ src/pager.c
@@ -64,7 +64,7 @@ struct pager {
     int * blocks_free_stack; /*guarda os indices de todos os blocos livres*/
 	pid_t *block2pid;
     int n_procs;
-	struct proc **pid2proc; /*lista que mapeia pids para as outras informações daquele processo(struct proc, com informações como n_pages, etc)*/
+	struct proc *pid2proc; /*lista que mapeia pids para as outras informações daquele processo(struct proc, com informações como n_pages, etc)*/
 };
 
 struct pager my_pager;
@@ -78,14 +78,14 @@ void second_chance()
         {
             for (int i = 0; i < my_pager.n_procs; i++)
             {
-                if (my_pager.pid2proc[i]->pid == my_pager.frames[second_chance_idx].pid)
+                if (my_pager.pid2proc[i].pid == my_pager.frames[second_chance_idx].pid)
                 {
-                    int frame_from = my_pager.pid2proc[i]->pages[my_pager.frames[second_chance_idx].page].frame;
-                    int block_to = my_pager.pid2proc[i]->pages[my_pager.frames[second_chance_idx].page].block;
-                    my_pager.pid2proc[i]->pages[my_pager.frames[second_chance_idx].page].on_disk = 1;
-                    my_pager.pid2proc[i]->pages[my_pager.frames[second_chance_idx].page].frame = -1;
+                    int frame_from = my_pager.pid2proc[i].pages[my_pager.frames[second_chance_idx].page].frame;
+                    int block_to = my_pager.pid2proc[i].pages[my_pager.frames[second_chance_idx].page].block;
+                    my_pager.pid2proc[i].pages[my_pager.frames[second_chance_idx].page].on_disk = 1;
+                    my_pager.pid2proc[i].pages[my_pager.frames[second_chance_idx].page].frame = -1;
                     mmu_disk_write(frame_from, block_to); 
-                    mmu_nonresident(my_pager.pid2proc[i]->pid, pager_page_to_addr(my_pager.frames[frame_from].page));
+                    mmu_nonresident(my_pager.pid2proc[i].pid, pager_page_to_addr(my_pager.frames[frame_from].page));
                 }
             }
             break;
@@ -107,10 +107,16 @@ void pager_init(int nframes, int nblocks){
   my_pager.nblocks = nblocks;
   my_pager.frames_free = nframes;
   my_pager.free_frames_stack = malloc(sizeof(int)*nframes);
+  int p = 0;
+  for (int i = nframes-1; i >=0 ; i--)
+  {
+    my_pager.free_frames_stack[p] = i;
+    p++;
+  }/*
   for (int i = 0; i < nframes; i++)
   {
     my_pager.free_frames_stack[i] = i;
-  }
+  }*/
   my_pager.blocks_free = nblocks;
   my_pager.blocks_free_stack = malloc(sizeof(int)*nblocks);
   for (int i = 0; i < nblocks; i++)
@@ -120,6 +126,7 @@ void pager_init(int nframes, int nblocks){
   my_pager.block2pid = malloc(nblocks*sizeof(pid_t));
   my_pager.clock = 0;
   my_pager.n_procs = 0;
+  my_pager.pid2proc = malloc(sizeof(struct proc));
   pthread_mutex_unlock(&my_pager.mutex);
 }
 
@@ -128,8 +135,9 @@ void pager_create(pid_t pid)
     pthread_mutex_trylock(&my_pager.mutex);
 	my_pager.n_procs++;
 	my_pager.pid2proc = realloc(my_pager.pid2proc, my_pager.n_procs*sizeof(struct proc));
-	my_pager.pid2proc[my_pager.n_procs-1]->pid = pid;
-    my_pager.pid2proc[my_pager.n_procs-1]->npages = 0;
+	my_pager.pid2proc[my_pager.n_procs-1].pid = pid;
+    my_pager.pid2proc[my_pager.n_procs-1].npages = 0;
+    my_pager.pid2proc[my_pager.n_procs-1].pages = malloc(sizeof(struct page_data));
     pthread_mutex_unlock(&my_pager.mutex);
 }
 
@@ -146,22 +154,22 @@ void *pager_extend(pid_t pid)
         int ind_page = 0;
         for (int i = 0; i < my_pager.n_procs; i++)
         {
-            if (my_pager.pid2proc[i]->pid == pid)
+            if (my_pager.pid2proc[i].pid == pid)
             {
-                ind_page = my_pager.pid2proc[i]->npages;
+                ind_page = my_pager.pid2proc[i].npages;
                 ind_pid2proc = i;
 
-                my_pager.pid2proc[i]->npages++;
-                if (my_pager.pid2proc[i]->npages==1)
+                my_pager.pid2proc[i].npages++;
+                if (my_pager.pid2proc[i].npages==1)
                 {
-                    my_pager.pid2proc[i]->pages = malloc(sizeof(struct page_data));
+                    my_pager.pid2proc[i].pages = malloc(sizeof(struct page_data));
                 }
                 else
                 {
-                    my_pager.pid2proc[i]->pages = realloc(my_pager.pid2proc[i]->pages, sizeof(struct page_data)*my_pager.pid2proc[i]->npages);
+                    my_pager.pid2proc[i].pages = realloc(my_pager.pid2proc[i].pages, sizeof(struct page_data)*my_pager.pid2proc[i].npages);
                 }
-                my_pager.pid2proc[i]->pages[ind_page].block = my_pager.blocks_free;
-                my_pager.pid2proc[i]->pages[0].on_disk = -1;
+                my_pager.pid2proc[i].pages[ind_page].block = my_pager.blocks_free;
+                my_pager.pid2proc[i].pages[ind_page].on_disk = 1;
                 break;
             }
         }
@@ -172,26 +180,26 @@ void *pager_extend(pid_t pid)
             int frame_idx = my_pager.free_frames_stack[my_pager.frames_free];
 
             my_pager.frames[frame_idx].pid = pid;
-            my_pager.pid2proc[ind_pid2proc]->pages[ind_page].frame = my_pager.frames_free;
+            my_pager.pid2proc[ind_pid2proc].pages[ind_page].frame = frame_idx;
+            my_pager.pid2proc[ind_pid2proc].pages[ind_page].on_disk = 0;
             my_pager.frames[frame_idx].dirty = 1;
-
-            mmu_resident(pid, pager_page_to_addr(frame_idx), frame_idx, PROT_NONE);
+            my_pager.frames[frame_idx].prot = PROT_NONE;
             
             pthread_mutex_unlock(&my_pager.mutex);
-            return pager_page_to_addr(frame_idx); 
+            return pager_page_to_addr(ind_page); 
         }
         else
         {
             //se não tem pagina livre roda o algoritmo da segunda chance e passa apgina a ser retirada para o disco
             second_chance();
-            my_pager.pid2proc[ind_pid2proc]->pages[ind_page].frame = second_chance_idx; //vai ter o valor certo ao final da execução do second_chance
-            
-            my_pager.frames[second_chance_idx].dirty = 1;
-            
-            mmu_resident(pid, pager_page_to_addr(second_chance_idx), second_chance_idx, PROT_NONE);
+            my_pager.pid2proc[ind_pid2proc].pages[ind_page].frame = second_chance_idx; //vai ter o valor certo ao final da execução do second_chance
+            my_pager.pid2proc[ind_pid2proc].pages[ind_page].on_disk = 0;
 
+            my_pager.frames[second_chance_idx].dirty = 1;
+            my_pager.frames[second_chance_idx].prot = PROT_NONE;
+            
             pthread_mutex_unlock(&my_pager.mutex);
-            return pager_page_to_addr(second_chance_idx);
+            return pager_page_to_addr(ind_page);
         }
     }
     pthread_mutex_unlock(&my_pager.mutex);
@@ -204,10 +212,10 @@ void pager_fault(pid_t pid, void *addr)
     int page = pager_addr_to_page(addr);
     for (int i = 0; i < my_pager.n_procs; i++)
     {
-        if (my_pager.pid2proc[i]->pid==pid)
+        if (my_pager.pid2proc[i].pid==pid)
         {
             //1) verificar se o endereço pertence ao processo (imprimir mensagem de erro --> seg fault)
-            if (page >= my_pager.pid2proc[i]->npages)
+            if (page >= my_pager.pid2proc[i].npages || page < 0)
             {
                 printf("Segmentation fault: address out of processes range");
                 exit(0);
@@ -217,18 +225,29 @@ void pager_fault(pid_t pid, void *addr)
             // se estiver dá permissão de escrita (chprot)
             // e se for o primeiro acesso àquela pagina precisa dar mmu_zero_fill
             
-            int frame = my_pager.pid2proc[i]->pages[page].frame;
-            if (my_pager.pid2proc[i]->pages[page].on_disk) //não está na RAM
+            int frame = my_pager.pid2proc[i].pages[page].frame;
+            if (my_pager.pid2proc[i].pages[page].on_disk) //não está na RAM
             {
-                second_chance();
-                my_pager.pid2proc[i]->pages[page].frame = second_chance_idx;
-                frame = second_chance_idx;
-                int block = my_pager.pid2proc[i]->pages[page].block;
-                mmu_zero_fill(frame);
-                mmu_disk_read(block, frame);
+                if (my_pager.frames_free>0)
+                {
+                    my_pager.frames_free--;
+                    my_pager.pid2proc[i].pages[page].frame = my_pager.free_frames_stack[my_pager.frames_free];
+                    frame = my_pager.frames_free;
+                }
+                else
+                {
+                    second_chance();
+                    my_pager.pid2proc[i].pages[page].frame = second_chance_idx;
+                    frame = second_chance_idx;
+                }
+                my_pager.frames[frame].prot = PROT_READ;
                 my_pager.frames[frame].dirty = 0;
                 my_pager.frames[frame].reference_bit = 1;
-                mmu_chprot(pid, addr, PROT_READ);
+                my_pager.pid2proc[i].pages[page].on_disk = 0;
+                int block = my_pager.pid2proc[i].pages[page].block;
+                mmu_zero_fill(frame);
+                mmu_resident(pid, pager_page_to_addr(page), frame, PROT_READ);
+                mmu_disk_read(block, frame);
             }
             else
             {
@@ -238,7 +257,17 @@ void pager_fault(pid_t pid, void *addr)
                     my_pager.frames[frame].dirty = 0;
                     my_pager.frames[frame].reference_bit = 1;
                 }
-                mmu_chprot(pid, addr, PROT_WRITE);
+
+                if (my_pager.frames[frame].prot==PROT_NONE)
+                {
+                    my_pager.frames[frame].prot = PROT_READ;
+                    mmu_resident(pid, pager_page_to_addr(page), frame, PROT_READ);
+                }
+                else
+                {
+                    my_pager.frames[frame].prot = PROT_READ | PROT_WRITE;
+                    mmu_chprot(pid, pager_page_to_addr(page), PROT_READ | PROT_WRITE);
+                }
             }
             
             break;
@@ -259,10 +288,10 @@ int pager_syslog(pid_t pid, void *addr, size_t len)
     int final_page = pager_addr_to_page((addr+len));
     for (int i = 0; i < my_pager.n_procs; i++)
     {
-        if (my_pager.pid2proc[i]->pid==pid)
+        if (my_pager.pid2proc[i].pid==pid)
         {
             //verificar se o endereço pertence ao processo (imprimir mensagem de erro --> seg fault)
-            if (initial_page >= my_pager.pid2proc[i]->npages || final_page >=my_pager.pid2proc[i]->npages)
+            if (initial_page >= my_pager.pid2proc[i].npages || final_page >=my_pager.pid2proc[i].npages)
             {
                 printf("Segmentation fault: address out of processes range");
                 pthread_mutex_unlock(&my_pager.mutex);
@@ -276,10 +305,11 @@ int pager_syslog(pid_t pid, void *addr, size_t len)
             {
                 for (; byte_atual < sysconf(_SC_PAGE_SIZE) && byte_atual+page*sysconf(_SC_PAGE_SIZE)<len ; byte_atual++)
                 {
-                    buf[page*sysconf(_SC_PAGE_SIZE)+byte_atual] = (char)pmem[my_pager.pid2proc[i]->pages[page].frame + byte_atual];
+                    buf[page*sysconf(_SC_PAGE_SIZE)+byte_atual] = (char)pmem[my_pager.pid2proc[i].pages[page].frame + byte_atual];
                     printf("%02x", (unsigned)buf[page*sysconf(_SC_PAGE_SIZE)+byte_atual]);
                 }
             }
+            printf("\n");
             pthread_mutex_unlock(&my_pager.mutex);
             return 0;
         }
@@ -293,15 +323,15 @@ void pager_destroy(pid_t pid)
     pthread_mutex_trylock(&my_pager.mutex);
     for (int i = 0; i < my_pager.n_procs; i++)
     {
-        if (my_pager.pid2proc[i]->pid == pid)
+        if (my_pager.pid2proc[i].pid == pid)
         {
-            my_pager.pid2proc[i]->pid = -1;
-            for (int j = 0; j < my_pager.pid2proc[i]->npages; j++)
+            my_pager.pid2proc[i].pid = -1;
+            for (int j = 0; j < my_pager.pid2proc[i].npages; j++)
             {
-                int bloco_liberado = my_pager.pid2proc[i]->pages[j].block;
+                int bloco_liberado = my_pager.pid2proc[i].pages[j].block;
                 my_pager.blocks_free++;
                 my_pager.blocks_free_stack[my_pager.blocks_free] = bloco_liberado;
-                int frame_liberado = my_pager.pid2proc[i]->pages[j].frame;
+                int frame_liberado = my_pager.pid2proc[i].pages[j].frame;
                 if (frame_liberado!=-1)
                 {
                     my_pager.frames_free++;
@@ -309,9 +339,9 @@ void pager_destroy(pid_t pid)
                 }
                 my_pager.block2pid[bloco_liberado] = -1;
             }
-            my_pager.pid2proc[i]->npages = 0;
-            my_pager.pid2proc[i]->maxpages = 0;
-            free(my_pager.pid2proc[i]->pages);
+            my_pager.pid2proc[i].npages = 0;
+            my_pager.pid2proc[i].maxpages = 0;
+            free(my_pager.pid2proc[i].pages);
             my_pager.n_procs--;
             break;
         }
